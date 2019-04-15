0c0aafc7eae2e5161cff220a98f0a6fc331e1798
erikyoung <erik.young@rmit.edu.vn>
2019-04-15 12:15:32 +0700

    added cities to display page

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import Tour from "../Tour";
+
+import './tourlist.scss'; 
+import {tourData} from '../../tourData';
+
+
 export default class TourList extends Component {
+
+state = {
+	tours: tourData
+}; 
+
+removeTour = id  => {
+	console.log(id); 
+};
+
+
   render() {
+  	const {tours} = this.state; 
+  	//console.log(this.state.tours); 
     return (
-      <div>
-        Hello From Tourslist
-        <Tour />
-      </div>
+    	
+      <section className="tourlist">
+      	{
+      		tours.map(tour => (
+      			<Tour key={tour.id} tour={tour} removeTour={this.removeTour}
+      			/>
+      			))}
+
+      </section>
     );
   }
 }
